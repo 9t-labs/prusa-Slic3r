f8833cbddd780029fe232dbafe1b8868905dc268
Luke Taverne <luke@9tlabs.com>
2019-05-10 16:30:15 -0400

    SK-105: Fix bed centering for single objects. Polygon::centroid() has a problem, using a workaround for now.

--- src/slic3r.cpp
+++ src/slic3r.cpp
@@ -378,7 +378,15 @@ int CLI::run(int argc, char **argv)
                 if (! m_config.opt_bool("dont_arrange")) {
                     //FIXME make the min_object_distance configurable.
                     model.arrange_objects(fff_print.config().min_object_distance());
-					model.center_instances_around_point(m_config.option<ConfigOptionPoint>("center")->value);
+                    if (m_print_config.has("bed_shape")) {
+                        const auto bed_shape = Slic3r::Polygon::new_scale(
+                                m_print_config.opt<ConfigOptionPoints>("bed_shape")->values);
+                        auto centroid = bed_shape.centroid().cast<double>();
+                        auto modifiedCentroid = Point(centroid.x() / 1000000, centroid.y() / 1000000); //FIXME: Why is this happening to the centroid?...
+                        model.center_instances_around_point(modifiedCentroid.cast<double>());
+                    } else {
+                        model.center_instances_around_point(m_config.option<ConfigOptionPoint>("center")->value);
+                    }
                 }
                 if (printer_technology == ptFFF) {
                     for (auto* mo : model.objects)
